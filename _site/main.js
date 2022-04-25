e6e1e911a746f84db0a97c05d6d6fb0e7eef3181
Toby Kind <sushidev2@gmail.com>
2022-04-25 17:27:50 -0400

    LOTS of stuff

--- _site/main.js
+++ _site/main.js
@@ -1,16 +1,7 @@
 $(function() {
-var document_height = $(document).height();
-var sidebar = $('.sidebar');
-var sidebarContainer = $('.sidebarContainer');
-var sidebarStretch = $('.sidebarStretch');
-var articleBox = $('.articleBox');
-var sidebar_height = sidebar.css("height");
-var bodyContent = $('.bodyContent');
+var sideImage = $('.sideImage');
+var parent = $(this).parent();
 
-if (document_height > sidebar_height) {
-    sidebar.css('height', document_height - 50);
-}
-sidebar.css('min-height', $( window ).height() - 70);
-sidebarContainer.css('height', document_height - 70);
+sideImage.css('top', (parent.height()/2));
 
 });
